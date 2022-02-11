da879da5f8eead837566fd5b2c4d2989676fe833
Seo Hyeon Bae <bae4614@gmail.com>
2022-02-11 20:25:27 +0900

    update

--- app-files/data.js
+++ app-files/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-kakaotalk_20220205_224356151",
-      "name": "KakaoTalk_20220205_224356151",
+      "id": "0-",
+      "name": "수로왕릉",
       "levels": [
         {
           "tileSize": 256,
@@ -12,16 +12,128 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
         }
       ],
-      "faceSize": 360,
+      "faceSize": 1344,
       "initialViewParameters": {
-        "yaw": -1.8337438627691132,
-        "pitch": 0,
-        "fov": 1.2344983363832869
+        "yaw": 0.07193915805173035,
+        "pitch": -0.019668147458105523,
+        "fov": 1.3156119066172163
       },
-      "linkHotspots": [],
-      "infoHotspots": []
+      "linkHotspots": [
+        {
+          "yaw": 1.0637758152898762,
+          "pitch": 0.1944409057102945,
+          "rotation": 1.5707963267948966,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.366198301014947,
+          "pitch": 0.03765819354185851,
+          "title": "수로왕릉",
+          "text": "경상남도 김해시 서상동에 있는 금관가야의 시조 수로왕의 무덤이다. 높이는 약 5m, 사적 제 73호이다. 왕릉은 원형봉토분이며, 능비와 상석, 문무인석, 마양호석 등의 석조물이 갖추어져 있다.<div><div class=\"autosourcing-stub-extra\" style=\"color: rgb(136, 136, 136); font-family: 나눔고딕, NanumGothic, &quot;맑은 고딕&quot;, &quot;malgun gothic&quot;, 돋움, Dotum, &quot;Apple SD Gothic Neo&quot;, Helvetica, sans-serif; font-size: 13.006px;\"><p style=\"font-style: normal; font-weight: normal; font-size: 12px; line-height: 1.714em; font-family: Dotum; margin-top: 11px; margin-bottom: 7px; color: black;\"><strong style=\"font-variant-ligatures: inherit; font-variant-caps: inherit; display: inline-block; padding: 0px 7px 0px 0px;\">[네이버 지식백과]</strong><span style=\"font-variant-ligatures: inherit; font-variant-caps: inherit;\">&nbsp;</span><a href=\"https://terms.naver.com/entry.naver?docId=558794\" style=\"background-color: rgba(58, 68, 84, 0.8); color: rgb(0, 51, 172); font-weight: 400; font-variant-ligatures: inherit; font-variant-caps: inherit;\">김해수로왕릉</a><span style=\"font-variant-ligatures: inherit; font-variant-caps: inherit;\">&nbsp;</span><span style=\"font-variant-ligatures: inherit; font-variant-caps: inherit;\">[金海首露王陵] (한국민족문화대백과, 한국학중앙연구원)</span></p></div></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "숭선전",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.47273026592773704,
+        "pitch": 0.010090213877148813,
+        "fov": 1.3156119066172163
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.29638504215930084,
+          "pitch": 0.24833893392098538,
+          "rotation": 9.42477796076938,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6728681280252289,
+          "pitch": 0.13230024424724185,
+          "title": "숭선전",
+          "text": "숭선전은 고종 21년 (1884)에 임금이 내린 이름으로 가락국 시조 수로왕과 왕후 허씨의 위패를 봉안하여 향화를 받는 전각이다. (경상남도 유형문화재)"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "숭모재",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.6139139913274967,
+        "pitch": -0.35232802989442824,
+        "fov": 1.3156119066172163
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.219982996611968,
+          "pitch": 0.262324896016473,
+          "rotation": 10.995574287564278,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.0701804560533539,
+          "pitch": 0.09323369721143848,
+          "title": "숭모재",
+          "text": "."
+        }
+      ]
     }
   ],
   "name": "Project Title",
